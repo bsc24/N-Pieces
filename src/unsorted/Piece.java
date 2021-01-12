cad510dbe11be3f83f2fb46defa51aeacacbb71b
bsc24 <bchin08@gmail.com>
2021-01-12 18:02:23 -0500

    Added a constructor and a static function to generate a random Piece.

--- src/unsorted/Piece.java
+++ src/unsorted/Piece.java
@@ -1,10 +1,44 @@
 package unsorted;
 
+import java.util.Random;
+
 public class Piece {
 	private final PieceType type;
 	
 	public Piece(PieceType type) {
 		this.type = type;
+	}
+	
+	public Piece(int num) {
+		num = num % 6;
+		switch(num) {
+		case 0:
+			this.type = PieceType.PAWN;
+			break;
+		case 1:
+			this.type = PieceType.KNIGHT;
+			break;
+		case 2:
+			this.type = PieceType.BISHOP;
+			break;
+		case 3:
+			this.type = PieceType.ROOK;
+			break;
+		case 4:
+			this.type = PieceType.QUEEN;
+			break;
+		case 5:
+			this.type = PieceType.KING;
+			break;
+		default:
+			this.type = null;
+			break;
+		}
+	}
+	
+	public static Piece randomPiece() {
+		Random r = new Random();
+		return new Piece(r.nextInt(6));
 	}
 	
 	public PieceType getType() {
